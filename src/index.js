5b5c2d903e8f60cfd8490f0feb6355959079e933
Fixitpanda <george.khananaev@gmail.com>
2022-09-09 23:01:48 +0300

    minor fixes

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-        <App />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App /> 
         </BrowserRouter>
     </React.StrictMode>
 );
