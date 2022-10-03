22074619eb40a5a6390e1338a883d3808df5f4d6
Fixitpanda <george.khananaev@gmail.com>
2022-10-03 06:13:03 +0300

    previous-next buttons added

--- Frontend/src/Pages/Home/HomeDataFetch.js
+++ Frontend/src/Pages/Home/HomeDataFetch.js
@@ -51,26 +51,28 @@ function AllItems() {
 
     // getting pages numbers, still need to make buttons "previous / next page" and the actual pages with fast-api.
     const getPagesNumbers = () => {
-        let content = [];
+        const content = [];
+        const previousButton = [];
+        const nextButton = [];
         for (let i = 1; i < numberOfPages + 1; i++) {
             const page = i;
 
             content.push(<li className="page-item" onClick={() => setActivePage(String(page))}><button className="page-link pointer">{page}</button></li>);
-            //
-            // <li className="page-item"> className="page-link" onClick={(e) => setActivePage(page) }" + item}>{item}</a></li>
         }
-        return content;
+        if (Number(activePage) > 1) {
+            previousButton.push(<li className="page-item"><button className="page-link"
+                                                                onClick={() => setActivePage(String(Number(activePage) - 1))}>Previous</button></li>);
+        } else if (Number(activePage) === 1) {
+            previousButton.push(<li className="page-item disabled"><button className="page-link">Previous</button></li>);
+        }
+        if (Number(numberOfPages) === Number(activePage)) {
+            nextButton.push(<li className="page-item disabled"><button className="page-link">Next</button></li>);
+        } else {
+            nextButton.push(<li className="page-item"><button onClick={() => setActivePage(String(Number(activePage) + 1))} className="page-link">Next</button></li>);
+        }
+        return [content, previousButton, nextButton];
     };
 
-
-
-    // index example for later
-    // function createPages2() {
-    //     let itemList=[];
-    //     items.forEach((item,index)=>{
-    //         itemList.push( <li key={index}>{item}</li>)
-    //     })
-    // }
 
     function searchItems(value) {
         let result = [...items];
@@ -134,18 +136,9 @@ function AllItems() {
 
                     <nav aria-label="...">
                         <ul className="pagination">
-                            <li className="page-item disabled">
-                                <span className="page-link">Previous</span>
-                            </li>
-                            {getPagesNumbers()}
-
-                            <li className="page-item disabled">
-                                <a className="page-link" href="/">Next</a>
-                            </li>
-
-
-
-
+                           {getPagesNumbers()[1]}  {/* previous*/}
+                            {getPagesNumbers()[0]} {/* pages */}
+                            {getPagesNumbers()[2]} {/* pages */}
                         </ul>
                     </nav>
                 </div>
